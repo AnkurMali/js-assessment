3dfd9449543a1f184dc5fae935935e61c3d6f073
Ankur Mali <ankur@Ankurs-Mac-Mini.local>
2015-01-28 15:56:20 +0530

    file:numbers/fumnction/objects/regex/modules made error free

--- app/functions.js
+++ app/functions.js
@@ -20,38 +20,59 @@ define(function() {
 
     makeClosures : function(arr, fn) {
 
-        /*var count = [];
+     var array = [];
 
-      var newfunction = function(val) {
+      var makeFn = function(val) {
         return function() { return fn(val); };
       };
 
-      for (var i = 0, i< arr.length;i++) {
-        count.push(newfunction(arr[i]));
+      for (var i = 0, len = arr.length; i < len; i++) {
+        array.push(makeFn(arr[i]));
       }
 
-      return count;*/
-  
+      return array;
     },
 
     partial : function(fn, str1, str2) {
+      return function(arg){
+        return fn.apply(undefined,[str1,str2,arg]);
+      };
 
     },
 
     useArguments : function() {
-
+      sum = 0;
+      for ( var i=0; i < arguments.length; i++)
+      {
+       sum = sum + arguments[i];
+      }
+    return sum;
     },
 
     callIt : function(fn) {
-
+       var args; 
+       args = Array.prototype.slice.call(arguments, 1, arguments.length);
+      fn.apply(undefined, args);
     },
 
     partialUsingArguments : function(fn) {
-
+      var args;
+      args = Array.prototype.slice.call(arguments, 1, arguments.length);
+      return function(){
+        var argsnew;
+        argsnew = args.concat(Array.prototype.slice.call(arguments));
+         return fn.apply(undefined, argsnew);
+      };
+      
     },
 
     curryIt : function(fn) {
 
+      var slice = Array.prototype.slice,
+      args = slice.apply(arguments, [1]);
+      return function () {
+      return fn.apply(undefined, args.concat(slice.apply(arguments)));
+      };
     }
   };
 });
