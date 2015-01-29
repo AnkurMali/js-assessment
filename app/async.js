797ad49d158909290e2a4e684719481cc1dd4ec5
Ankur Mali <ankur@Ankurs-Mac-Mini.local>
2015-01-29 12:43:40 +0530

    file: async.js modified and successfully created connection with server to collect data

--- app/async.js
+++ app/async.js
@@ -3,11 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+     var new1 = $.Deferred();
+     setTimeout(function(){
+     	new1.resolve(value);
+     },10);
+     	return new1.promise();
     },
 
     manipulateRemoteData : function(url) {
+       var new2 = $.Deferred();
 
+       $.ajax(url).then(function(response){
+       	var people = $.map(response.people,function(person){
+       		return person.name;
+       	});
+       	new2.resolve(people.sort());
+       });
+       return new2.promise();
     }
   };
 });
